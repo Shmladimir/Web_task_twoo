84954893d5b2f36069f4e60cead9990e74548a81
ShmalodyaTest <legiion95007@gmail.com>
2020-10-12 00:14:45 +1100

    phpAddBegin

---
+++ src/nav.php
@@ -0,0 +1,9 @@
+<div class="top_nav">
+            <ul>
+                <li><a href="#" class="sakhguLogo"></a></li>
+                <li><a href="#" class="main_page nav_hover">Главная</a></li>
+                <li><a href="#" class="my_page nav_hover">My Page</a></li>
+                <li><a href="#" class="schedule nav_hover">Расписание</a></li>
+                <li><a href="#" class="about_teacher nav_hover">О преподавателях</a></li>
+            </ul>   
+</div>
